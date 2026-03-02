d9467ff186bf2f99497889775dc43c979da75a43
codefreezing <3102637943@qq.com>
2026-03-02 19:55:18 +0800

    实现了后端的登录和注册功能

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 
 const bodyParser = require('body-parser')
 
+const Joi = require('joi')
+
 const cors = require('cors')
 app.use(cors())
 
@@ -14,9 +16,39 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+	// status=0为成功,=1为失败,默认设为1,方便处理失败的情况
+	res.cc = (err, status = 1) => {
+		res.send({
+			status,
+			// 判断这个error是错误对象还是字符串
+			message: err instanceof Error ? err.message : err,
+		})
+	}
+	next()
+})
+
+const jwtconfig = require('./jwt_config/index.js')
+const { expressjwt: jwt} = require('express-jwt')
+app.use(jwt({
+	secret:jwtconfig.jwtSecretKey,algorithms:['HS256']
+}).unless({
+	path:[/^\/api\//]
+}))
+
 // 导入路由模块
 const loginRouter = require('./router/login')
 app.use('/api', loginRouter)
+
+// 对不符合joi规则的情况进行报错
+app.use((err,req, res, next) => {
+	if (err instanceof Joi.ValidationError){
+		res.send({
+			status: 1,
+			message:'输入的数据不符合验证规则'
+		})
+	}
+})
 
 // 绑定和监听指定端口
 app.listen(3007, () => {
